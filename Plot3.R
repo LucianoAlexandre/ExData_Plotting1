ba421cc4dba39cd22a999eac7779048b0fee572b
LucianoAlexandre <lucianoalexandre@globo.com>
2015-01-10 12:18:15 -0200

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,27 @@
+setwd("~/ExploratoryDataProject/")
+
+if (is.null(data)){
+  data<-read.table("household_power_consumption.txt",
+                 header = TRUE,
+                 sep = ";",
+                 colClasses=c("character", "character", rep("numeric",7)),
+                 na="?")
+
+  data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+  data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+  dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+  data <- subset(data, Date %in% dates)
+}
+
+png("plot3.png", width=480, height=480)
+plot(data$Time, data$Sub_metering_1, type="l", col="black",
+     xlab="", ylab="Energy sub metering")
+lines(data$Time, data$Sub_metering_2, col="red")
+lines(data$Time, data$Sub_metering_3, col="blue")
+legend("topright",
+       col=c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=1)
+
+dev.off()
